9d5b8fbd31d944ec9058051435480dbd58ffbb9c
Justin Love <git@JustinLove.name>
2018-07-14 12:01:00 -0500

    fix spawn layer

---
+++ Gruntfile.js
@@ -0,0 +1,64 @@
+var spec = require('./lib/spec')
+var prompt = require('prompt')
+prompt.start()
+
+console.log('run')
+
+var modPath = '../../server_mods/com.wondible.pa.hermes_oribital_spawn/'
+var stream = 'stable'
+var media = require('./lib/path').media(stream)
+
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    copy: {
+      mod: {
+        files: [
+          {
+            src: [
+              'modinfo.json',
+              'LICENSE.txt',
+              'README.md',
+              'CHANGELOG.md',
+              'ui/**',
+              'pa/**'],
+            dest: modPath,
+          },
+        ],
+      },
+    },
+    // copy files from PA, transform, and put into mod
+    proc: {
+      // form 1: just the relative path, media src is assumed
+      hermes: {
+        src: [
+          'pa*/units/orbital/orbital_probe/orbital_probe.json'
+        ],
+        cwd: media,
+        dest: 'pa/units/orbital/orbital_probe/orbital_probe.json',
+        process: function(spec) {
+          spec.spawn_layers = "WL_Orbital"
+          return spec
+        }
+      },
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerMultiTask('proc', 'Process unit files into the mod', function() {
+    if (this.data.targets) {
+      var specs = spec.copyPairs(grunt, this.data.targets, media)
+      spec.copyUnitFiles(grunt, specs, this.data.process)
+    } else {
+      var specs = this.filesSrc.map(function(s) {return grunt.file.readJSON(media + s)})
+      var out = this.data.process.apply(this, specs)
+      grunt.file.write(this.data.dest, JSON.stringify(out, null, 2))
+    }
+  })
+
+  // Default task(s).
+  grunt.registerTask('default', ['proc', 'copy:mod']);
+
+};
+
---
+++ README.md
@@ -0,0 +1,15 @@
+# Hermes Orbital Spawn
+
+The Hermes spawns on the ground when using debug spawn. This mod changes the spawn layer so it can start orbital.
+
+## Development
+
+The generated project includes a `package.json` that lists the dependencies, but you'll need to run `npm install` to download them.
+
+PA will upload **all files** in the mod directory, including `node_modules` and maybe even `.git` - you probably don't want to use this in `server_mods` directly, unless you really like waiting.  The template is set up run to run as a project within a peer directory of `server_mods` - I use `server_mods_dev/mod_name`.  The task `grunt copy:mod` will copy the mod files to `../../server_mods/identifier`, you can change the `modPath` in the Gruntfile if you want to run it from somewhere else.
+
+### Available Tasks
+
+- proc - modify nuke and antinuke units to preloaded versions
+- copy:mod - copy the mod files into server_mods
+- default: proc, copy:mod
